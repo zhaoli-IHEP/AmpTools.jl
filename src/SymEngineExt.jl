4751808f4029354a81517dd24fb08509c1ec2ff9
zhaoli-IHEP <zhaoli@ihep.ac.cn>
2022-12-14 14:33:21 +0800

    generate project files

---
+++ src/SymEngineExt.jl
@@ -0,0 +1,29 @@
+__precompile__()
+
+
+module SymEngineExt
+
+using SymEngine
+
+
+
+include("Type.jl")
+
+export is_FunctionSymbol
+
+
+
+
+
+
+###################
+function __init__()
+###################
+  return nothing
+end # function __init__
+
+end # module SymEngineExt
+
+
+
+
---
+++ src/Type.jl
@@ -0,0 +1,12 @@
+
+####################
+function is_FunctionSymbol(
+    expr::Basic
+)::Bool
+####################
+
+  return SymEngine.get_symengine_class(expr) == :FunctionSymbol
+
+end # function is_FunctionSymbol
+
+
