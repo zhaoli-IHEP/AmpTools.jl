b82ec20b63c22a3e62a04498796f2e2dd8d1a064
zhaoli-IHEP <zhaoli@ihep.ac.cn>
2023-02-19 01:45:15 +0800

    update get_det and get_adj

--- src/BasicMatrix.jl
+++ src/BasicMatrix.jl
@@ -1,6 +1,6 @@
 
 ############################################
-function get_det( MM::Matrix{Basic} )::Basic
+function get_det_old( MM::Matrix{Basic} )::Basic
 ############################################
 
   nr, nc = size(MM)
@@ -19,13 +19,45 @@ function get_det( MM::Matrix{Basic} )::Basic
 
   return detMM
 
+end # function get_det_old
+
+
+############################################
+function get_det( MM::Matrix{Basic} )::Basic
+############################################
+
+  nr, nc = size(MM)
+  @assert nr == nc
+
+  if nr == 1
+    return MM[1,1]
+  end # if
+
+  if nr == 2 
+    return MM[1,1]*MM[2,2] - MM[1,2]*MM[2,1]
+  end # if
+  
+  # larger than 2x2 uses the Laplace expansion
+  detMM = zero(Basic)
+
+  for cc in 1:nc
+    element = MM[1,cc]
+    cofactor = (-1)^(1+cc)
+    sub_mat = MM[ 2:nr, setdiff(1:nc,cc) ]
+    detMM += element * cofactor * get_det(sub_mat)
+  end # for cc
+
+  return detMM
+
 end # function get_det
 
 
 
+
 ####################################################
-function get_adj( MM::Matrix{Basic} )::Matrix{Basic}
+function get_adj_old( MM::Matrix{Basic} )::Matrix{Basic}
 ####################################################
+
   nr, nc = size( MM )
   @assert nr == nc
 
@@ -59,7 +91,47 @@ function get_adj( MM::Matrix{Basic} )::Matrix{Basic}
 
   return adjMM
 
+end # function get_adj_old
+
+
+
+
+
+
+
+####################################################
+function get_adj( MM::Matrix{Basic} )::Matrix{Basic}
+####################################################
+
+  nr, nc = size( MM )
+  @assert nr == nc
+
+  if nr == 1
+    return one(Basic)
+  end # if
+
+  if nr == 2
+    adjMM = Matrix{Basic}(undef,nr,nc)
+    adjMM[1,1] = MM[2,2]
+    adjMM[1,2] = -MM[1,2]
+    adjMM[2,1] = -MM[2,1]
+    adjMM[2,2] = MM[1,1]
+    return adjMM
+  end # if
+
+  # for larger matrix, notice there should a transpose.
+  adjMM = Matrix{Basic}(undef,nr,nc)
+  for rr in 1:nr, cc in 1:nc
+    cofactor = (-1)^(rr+cc)
+    sub_mat = MM[ setdiff(1:nr,cc), setdiff(1:nc,rr) ]
+    adjMM[rr,cc] = cofactor * get_det(sub_mat)
+  end # for rr, cc
+  
+  return adjMM
+
 end # function get_adj
+
+
 
 
 
